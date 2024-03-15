ecdcc08dfcb57d458c610dc253d22087f21cb134
DallasJr <49690481+DallasJr@users.noreply.github.com>
2024-03-15 12:13:25 +0100

    added main menu

--- Sketch.js
+++ Sketch.js
@@ -9,81 +9,101 @@ const enemyImages = [];
 const alliesImages = [];
 let maxHealth = 50;
 
+let inGame = false;
+
 function setup() {
     createCanvas(windowWidth-20, windowHeight-20);
+    canvas = document.querySelector('canvas');
+    canvas.style.display = 'none';
+}
+
+function startGame() {
+    let mainMenu = document.getElementById("main-menu");
+    mainMenu.style.display = 'none';
+    canvas.style.display = 'block';
     character = new Character();
+    inGame = true;
 }
 
 function draw() {
-    background(220);
-    text("Ennemies: " + enemies.length, 25, 30);
-    text("Allies: " + allies.length, 25, 50);
-    text("enemySpawnRate: " + enemySpawnRate, 25, 70);
-    text("lastSpawnTime: " + lastSpawnTime, 25, 90);
-    text("Level: " + character.level, 25, 130);
-    text("HP: " + character.hp, 25, 150);
-    text("Attack: " + character.getAttack(), 25, 170);
+    if (inGame) {
+        background(220);
+        text("Ennemies: " + enemies.length, 25, 30);
+        text("Allies: " + allies.length, 25, 50);
+        text("enemySpawnRate: " + enemySpawnRate, 25, 70);
+        text("lastSpawnTime: " + lastSpawnTime, 25, 90);
+        text("Level: " + character.level, 25, 130);
+        text("HP: " + character.hp, 25, 150);
+        text("Attack: " + character.getAttack(), 25, 170);
 
-    let currentTime = millis();
-    if (enemies.length < maxEnemies && currentTime - lastSpawnTime > enemySpawnRate) {
-        enemies.push(new Enemy(Math.ceil(random(1, 10)))); // Random tier between 1 and 10
-        lastSpawnTime = currentTime;
-    }
-    character.move();
-    character.display();
-
-    // Handle enemy logic
-    for (let i = enemies.length - 1; i >= 0; i--) {
-        let enemy = enemies[i];
-        enemy.display();
-        if (enemy.hp <= 0) {
-            enemies.splice(i, 1); // Remove enemy if defeated
+        let currentTime = millis();
+        if (enemies.length < maxEnemies && currentTime - lastSpawnTime > enemySpawnRate) {
+            enemies.push(new Enemy(Math.ceil(random(1, 10)))); // Random tier between 1 and 10
+            lastSpawnTime = currentTime;
         }
-    }
+        character.move();
+        character.display();
 
-    // Handle ally logic
-    for (let i = allies.length - 1; i >= 0; i--) {
-        let ally = allies[i];
-        ally.update();
-        ally.display();
-        if (ally.lifetime <= 0) {
-            allies.splice(i, 1); // Remove ally if time over
-        }
-    }
-
-    // Collision detection summon
-    enemies.forEach((enemy, index) => {
-        //text("HP: " + enemy.hp, enemy.x, enemy.y + 20);
-        if (dist(character.x, character.y, enemy.x, enemy.y) < 50) { // attack range
-            character.hp -= enemy.attack;
-            enemy.hp -= character.getAttack(); // Character attacks enemy
+        // Handle enemy logic
+        for (let i = enemies.length - 1; i >= 0; i--) {
+            let enemy = enemies[i];
+            enemy.display();
             if (enemy.hp <= 0) {
-                enemies.splice(index, 1); // Remove enemy if defeated
-                character.hp = maxHealth;
-                character.level++;
-            }
-            if (character.hp <= 0) {
-                character.reset();
-                enemies = []
+                enemies.splice(i, 1); // Remove enemy if defeated
             }
         }
 
-        // Enemy aggro and attack logic
-        /*if (dist(character.x, character.y, enemy.x, enemy.y) < 100) { // Aggro range
-            enemy.attackCharacter(character);
-        }*/
-    });
+        // Handle ally logic
+        for (let i = allies.length - 1; i >= 0; i--) {
+            let ally = allies[i];
+            ally.update();
+            ally.display();
+            if (ally.lifetime <= 0) {
+                allies.splice(i, 1); // Remove ally if time over
+            }
+        }
 
-    allies.forEach(ally => {
+        // Collision detection summon
         enemies.forEach((enemy, index) => {
-            if (dist(ally.x, ally.y, enemy.x, enemy.y) < 30) { // Ally attack range
-                enemy.hp -= ally.tier; // Assuming ally's tier is its attack power
+            //text("HP: " + enemy.hp, enemy.x, enemy.y + 20);
+            if (dist(character.x, character.y, enemy.x, enemy.y) < 50) { // attack range
+                character.hp -= enemy.attack;
+                enemy.hp -= character.getAttack(); // Character attacks enemy
                 if (enemy.hp <= 0) {
-                    enemies.splice(index, 1); // Remove enemy if defeated by ally
+                    enemies.splice(index, 1); // Remove enemy if defeated
+                    character.hp = maxHealth;
+                    character.level++;
+                }
+                if (character.hp <= 0) {
+                    character.reset();
+                    enemies = []
                 }
             }
+
+            // Enemy aggro and attack logic
+            /*if (dist(character.x, character.y, enemy.x, enemy.y) < 100) { // Aggro range
+                enemy.attackCharacter(character);
+            }*/
         });
-    });
+
+        allies.forEach(ally => {
+            enemies.forEach((enemy, index) => {
+                if (dist(ally.x, ally.y, enemy.x, enemy.y) < 30) { // Ally attack range
+                    enemy.hp -= ally.tier; // Assuming ally's tier is its attack power
+                    if (enemy.hp <= 0) {
+                        enemies.splice(index, 1); // Remove enemy if defeated by ally
+                    }
+                }
+            });
+        });
+    } else {
+        clear();
+        drawMainMenu();
+    }
+}
+
+function drawMainMenu() {
+
 }
 
 function preload() {
